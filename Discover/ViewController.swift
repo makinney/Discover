361f8e6ee049bd70c617ea2979cedbb78269d569
Michael Kinney <michaelakinney@comcast.net>
2015-05-16 13:07:06 -0700

    Initial Commit

---
+++ Discover/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Discover
+//
+//  Created by Michael Kinney on 5/16/15.
+//  Copyright (c) 2015 mkinney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
